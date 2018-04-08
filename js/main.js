8e00d8f21f213dce6b6b356fcf6af0f89acbb132
smontoro <sarahjmontoro@gmail.com>
2018-04-07 18:35:03 -0700

    added buy link button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 function bookSearch() {
 	var search = document.getElementById("search").value
-	document.getElementById("results").innerHTML = ""
-	console.log(search)
 
 	$.ajax({
 		url:"https://www.googleapis.com/books/v1/volumes?q=" + search,
@@ -11,15 +9,38 @@ function bookSearch() {
 
 			var results = document.getElementById("results")
 			for(i=0; i < data.items.length; i++) {
-				results.innerHTML += '<h1>' + data.items[i].volumeInfo.title + 
-				'</h1>' + 
-				'<h2>' + 
-				data.items[i].volumeInfo.authors + 
-				'</h2>' + 
-				data.items[i].volumeInfo.maturityRating +
-				'<h3>'
-				data.items[i].volumeInfo.publisher
-				'</h3>'
+				//store the data in variables
+				var photo = data.items[i].volumeInfo.imageLinks.thumbnail
+				var title = data.items[i].volumeInfo.title
+				var authors = data.items[i].volumeInfo.authors
+				var pubDate = data.items[i].volumeInfo.publishedDate 
+				var link = data.items[i].saleInfo.buyLink
+
+				//store HTML elements in variables
+				var photoImg = document.createElement("img")
+					photoImg.setAttribute("src", photo)
+				var titleH1 = document.createElement("h1")
+				var authorsH2 = document.createElement("h2")
+				var pubDateH3 = document.createElement("h3")
+				var button = document.createElement("button")
+					button.innerHTML = "Purchase Book"
+					button.setAttribute("style", "display: block; margin: 0 auto 50px auto;")
+				var linkA = document.createElement("a")
+					linkA.setAttribute("href", link)
+					linkA.appendChild(button)
+					linkA.setAttribute("target", "_blank")
+				
+
+				//append data variables to HTML variables
+				photoImg.innerHTML= photo
+				titleH1.innerHTML = title
+				authorsH2.innerHTML= authors
+				pubDateH3.innerHTML = pubDate
+
+				//append HTML variables to DOM
+				var results = document.getElementById("results")
+
+				results.innerHTML += photoImg.outerHTML + titleH1.outerHTML + authorsH2.outerHTML + pubDateH3.outerHTML + linkA.outerHTML;		
 
 			}
 		},
